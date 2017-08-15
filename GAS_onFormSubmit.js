66c6023a2c68f613c0217da69ab1d7c132e91a15
Joseph McLauchlan <mclauchlanjoseph@gmail.com>
2017-08-15 14:16:11 -0400

    Update GAS_onFormSubmit.js

--- GAS_onFormSubmit.js
+++ GAS_onFormSubmit.js
@@ -1,13 +1,12 @@
 function sendFormByEmail(e) 
 {    
   // Remember to replace this email address with your own email address
-  var email = "you@example.com"; 
+  var email = "joseph.r.mclauchlan.mil@mail.mil"; 
 
   var s = SpreadsheetApp.getActiveSheet();
   var headers = s.getRange(1,1,1,s.getLastColumn()).getValues()[0];    
   var message = "";
-  // Change subject line here
-  var subject = "New Hire: ";
+  var subject = "New Lead: ";
 
   // The variable e holds all the form values in an array.
   // Loop through the array and append values to the body.
@@ -16,12 +15,13 @@ function sendFormByEmail(e)
     message += headers[i] + ': '+ e.namedValues[headers[i]].toString() + "\n\n";     
  
   // Insert variables from the spreadsheet into the subject.
-  // In this case, I wanted the new hire's name and start date as part of the
-  // email subject. These are the 3rd and 16th columns in my form.
-  // This creates an email subject like "New Hire: Jane Doe - starts 4/23/2013"
-  subject += e.namedValues[headers[2]].toString() + " - starts " + e.namedValues[headers[15]].toString();
+  // In this case, I wanted the new personss name and date submitted as part of the
+  // email subject. These are the 1st and 2nd columns in my form.
+  // This creates an email subject like "New Lead: Jane Doe - submitted 4/23/2013"
+  subject += e.namedValues[headers[1]].toString() + " - submitted " + e.namedValues[headers[0]].toString();
 
   // Send the email
-  MailApp.sendEmail(email, subject, message); 
-  //Ensure you change the trigger to sendFormByEmail based on spreadsheet onFormSubmit
+  MailApp.sendEmail(email, subject, message);
+  Logger.log('Email has been sent!');
+  // Ensure you change the trigger to sendFormByEmail based on spreadsheet onFormSubmit
 }
